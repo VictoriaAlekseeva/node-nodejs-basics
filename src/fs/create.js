686a60178e18f7a781f5d7b5f4af36ff2fd2150b
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-25 01:14:21 +0100

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { promises, constants } from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createdFilePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await promises.access(createdFilePath, constants.F_OK)
+      .then(() => { throw new Error('FS operation failed') }
+      );
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await promises.writeFile(createdFilePath, 'I am fresh and young');
+    } else console.error(err.message)
+  }
 };
 
 await create();
