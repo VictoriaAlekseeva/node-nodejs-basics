288f6e96b4135221261e909056d2a75b8609a9d9
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-27 21:34:46 +0100

    feat: add solution for cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import { argv } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
-};
+    const envVariables = process.env;
+    const prefixVariablesRSS = Object.entries(envVariables).filter(entry => entry[0].startsWith('RSS_'))
+    const prefixVariablesFormatted = prefixVariablesRSS.map(entry => entry.join('=')).join('; ')
+    console.log(prefixVariablesFormatted);
+}
 
 parseEnv();
