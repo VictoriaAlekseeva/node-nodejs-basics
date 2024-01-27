d2072bd49c4aa0ef4e1526b06f2d94c6d65be5fb
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-27 21:50:17 +0100

    refactor: solution for cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2).reduce((acc, item, index, args) => {
+        if (item.startsWith('--')) acc.push(`${item} is ${args[index + 1]}`)
+        return acc;
+    }, []).join(', ')
+
+    console.log(args)
 };
 
 parseArgs();
