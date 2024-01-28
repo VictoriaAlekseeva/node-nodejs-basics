760408998c4b6848b0de5426ed988848b33715d8
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-28 18:48:47 +0100

    feat: add solution for wt/main.js and wt/worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import {Worker} from 'worker_threads';
+import path from 'path';
+import os from 'os';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToWorker = path.join(__dirname, 'worker.js');
+
+const CPUQuantity = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+    const results = []
+    const initialNum = 10;
+    for (let i = 0; i < CPUQuantity; i++ ) {
+        const worker = new Worker(pathToWorker, {workerData: initialNum + i});
+
+        const workerPromise = new Promise((resolve) => {
+            worker.on('message', (result) => resolve(result));
+            worker.on("error", (err) => resolve({ status: 'error', data: null}));
+        })
+        results.push(workerPromise)
+    }
+
+    const result = await Promise.all(results);
+    console.log(result)
 };
 
 await performCalculations();
