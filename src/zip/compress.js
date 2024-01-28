2b30fed3114125656bea5cd27d306e5f457a1ce2
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-28 02:48:43 +0100

    feat: add solution for zip/compress.js zip/decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToCompress = path.join(__dirname, 'files', 'fileToCompress.txt');
+const compressedFile = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+
+    const gzip = createGzip();
+    const source = createReadStream(fileToCompress);
+    const destination = createWriteStream(compressedFile);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const decompressedFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+const fileToDecompress = path.join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+
+    const gunzip = createGunzip();
+    const source = createReadStream(fileToDecompress);
+    const destination = createWriteStream(decompressedFile);
+
+    pipeline(source, gunzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();
