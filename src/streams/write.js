3a2e104914607f9c75df484516fce9acccfeb721
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-28 02:16:14 +0100

    feat: add solution for streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    console.log('press Ctrl + D or Ctrl + Z to exit')
+    const writableStream = createWriteStream(fileToWrite);
+    process.stdin.pipe(writableStream);
+    process.stdin.on('end', () => {
+        console.log('Text added to fileToWrite.txt');
+        process.exit();
+    });
 };
 
 await write();
