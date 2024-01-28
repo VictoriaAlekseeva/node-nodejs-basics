db7f8e0109ad1d8f8df5e63e5a36d6d86a10e6ca
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-01-28 01:35:01 +0100

    refactor: remove unused variables from solution for streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,10 @@
 import { createReadStream } from "fs";
-import { stdout } from "process";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// const sourceFolder = path.join(__dirname, 'files');
 const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
 
 const read = async () => {
